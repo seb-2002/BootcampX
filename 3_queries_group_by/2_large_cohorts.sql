8791a98ffba8acb468b41c64930229a2e3568a32
Sebastian Kann <sebastiankann@gmail.com>
2020-10-12 22:04:30 +0000

    3_queries_group_by

---
+++ 3_queries_group_by/2_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name AS cohort_name, 
+COUNT(students.id) AS student_count
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+HAVING COUNT(students.id) > 17
+ORDER BY student_count;
---
+++ 3_queries_group_by/4_average_completion_time.sql
@@ -0,0 +1,6 @@
+SELECT name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+WHERE end_date IS NULL
+GROUP BY name
+ORDER BY average_assignment_duration DESC; 
